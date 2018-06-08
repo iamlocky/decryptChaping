24e0de597a0bb66666eda7e2cc2a03b181dacf3f
iamlocky <iamlocky@qq.com>
2018-06-08 21:12:08 +0800

    优化代码

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,54 @@
+
+
 public class Main {
     /**
      * locky luo
      * 2018.6.8
      */
     public static void main(String[] args) {
-        String encrypt="476865786A776B687A72756F677A6C776B466B64736C716A24";
+        String encrypted = "476865786A776B687A72756F677A6C776B466B64736C716A24";
+        decryptCaesar(hex2Decimal(encrypted), null);
+    }
 
-        int[] encryptAscii=new int[encrypt.length()/2];
-        for (int i = 0; i <=encrypt.length()-2 ; i++) {
-            if (i%2==0){
-                encryptAscii[i/2]=(char) Integer.parseInt(encrypt.substring(i,i+2),16);
+    public static int[] hex2Decimal(String encrypted) {
+        int[] encryptAscii = new int[encrypted.length() / 2];
+        for (int i = 0; i <= encrypted.length() - 2; i++) {
+            if (i % 2 == 0) {
+                encryptAscii[i / 2] = Integer.parseInt(encrypted.substring(i, i + 2), 16);
             }
         }
+        return encryptAscii;
+    }
 
-        char[] result=new char[encrypt.length()];
-        for (int i = 0; i <127 ; i++) {
-            for (int j = 0; j <encryptAscii.length ; j++) {
-                for (int k = 0; k <127 ; k++) {
-                    if (encryptAscii[j]==k){
-                        int move=(k+i);
-                        move=move<0?(127+move):move;
-                        move=move>127?(move-127):move;
-                        result[j]=(char)move;
+    public static void decryptCaesar(int[] encryptAscii, Integer key) {
+        char[] result = new char[encryptAscii.length];
+        if (key == null) {//无位移密码，暴力破解
+            for (int i = 32; i < 127; i++) {
+                boolean flag = true;
+                for (int j = 0; j < encryptAscii.length && flag; j++) {
+                    int move = (encryptAscii[j] + i);
+                    move = move % 127;
+                    if (move < 33 || move > 126) {
+                        flag = false;
+                        continue;
                     }
+                    result[j] = (char) move;
                 }
+                if (flag) {
+                    print(new String(result));
+                }
+            }
+        } else {
+            for (int j = 0; j < encryptAscii.length; j++) {
+                int move = (encryptAscii[j] + key);
+                move = move % 127;
+                result[j] = (char) move;
             }
             print(new String(result));
         }
-
     }
 
-    public static void print(Object o){
+    public static void print(Object o) {
         System.out.println(o);
     }
 }
